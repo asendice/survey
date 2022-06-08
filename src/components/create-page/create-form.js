9f04b9676bca4a42128431b1044ac56d961c6a57
Dylan Travis <dylan.travis23@outlook.com>
2022-06-08 14:30:29 -0700

    name of survey css and state

--- src/components/create-page/create-form.js
+++ src/components/create-page/create-form.js
@@ -3,14 +3,13 @@ import { useState } from "react";
 import AddQuestion from "./add-question";
 
 function CreateForm(){
-  const [ name, setName ] = useState("");
+  const [ name, setName ] = useState("Untitled Survey");
   const [ questions, setQuestions ] = useState([]);
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>Create Survey</h1>
       <form className={classes.form}>
-        <label>Suvey Name:</label>
-        <input type="text" placeholder="enter name of survey" />
+        <h1 className={classes.name}>{name}</h1>
         <AddQuestion />
       </form>
     </div>
