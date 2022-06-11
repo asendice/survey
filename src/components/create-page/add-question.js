d7a40c7b8c68f8e993a1b10470c63cc2903defe0
Dylan Travis <dylan.travis23@outlook.com>
2022-06-11 15:34:35 -0700

    add question

--- src/components/create-page/add-question.js
+++ src/components/create-page/add-question.js
@@ -1,6 +1,14 @@
 import classes from "./add-question.module.css";
 import { useState } from "react";
-import { BsFillCaretDownFill, BsListUl, BsCheck2Square, BsTextareaResize, BsInputCursorText } from "react-icons/bs";
+import {
+  BsFillCaretDownFill,
+  BsListUl,
+  BsCheck2Square,
+  BsTextareaResize,
+  BsInputCursorText,
+  BsPlusCircle
+} from "react-icons/bs";
+import Button from "../utils/button";
 
 function AddQuestion() {
   const [open, setOpen] = useState(false);
@@ -14,20 +22,48 @@ function AddQuestion() {
 
   return (
     <div className={classes.container}>
-      <h2>Q1</h2>
-      <div className={classes.inputs}>
-        <input type="text" placeholder="Enter your question" className={classes.questionInput}/>
-        <div className={classes.dropdown} onClick={() => setOpen(!open)}>
-          <p>{type}</p> <BsFillCaretDownFill />
+      <div className={classes.content}>
+        <h2>Q1</h2>
+        <div className={classes.inputs}>
+          <input
+            type="text"
+            placeholder="Enter your question"
+            className={classes.questionInput}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className={classes.dropdown} onClick={() => setOpen(!open)}>
+            <p>{type}</p> <BsFillCaretDownFill />
+          </div>
+          {open && (
+            <div className={classes.dropdownContent}>
+              <ul>
+                <li onClick={() => updateType("Single Textfield")}>
+                  <BsInputCursorText />
+                  <p>Single Textfield</p>
+                </li>
+                <li onClick={() => updateType("Comment Textfield")}>
+                  <BsTextareaResize />
+                  <p>Comment Textfield</p>
+                </li>
+                <li onClick={() => updateType("Multiple Choice")}>
+                  <BsListUl />
+                  <p>Multiple Choice</p>
+                </li>
+                <li onClick={() => updateType("Checkbox")}>
+                  <BsCheck2Square />
+                  <p>Checkbox</p>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
-        {open && <div className={classes.dropdownContent}>
-          <ul>
-            <li onClick={() => updateType("Single Textfield")}><BsInputCursorText /><p>Single Textfield</p></li>
-            <li onClick={() => updateType("Comment Textfield")}><BsTextareaResize /><p>Comment Textfield</p></li>
-            <li onClick={() => updateType("Multiple Choice")}><BsListUl /><p>Multiple Choice</p></li>
-            <li onClick={() => updateType("Checkbox")}><BsCheck2Square /><p>Checkbox</p></li>
-          </ul>
-        </div>}
+      </div>
+      <div className={classes.actionBtns}>
+        <div className={classes.nextquestion}><BsPlusCircle /><p>Next Question</p></div>
+        <div className={classes.savencancel}>
+          <Button cancel={true}>CANCEL</Button>
+          <Button>SAVE</Button>
+        </div>
       </div>
     </div>
   );
